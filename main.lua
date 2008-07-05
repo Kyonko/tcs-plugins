43be8e21ce9eb8371bb57593201dd8b7c12418c8
Tyler Gibbons <calaihm@gmail.com>
2008-07-05 16:20:39 -0700

    I always forget to update the damn version count

--- main.lua
+++ main.lua
@@ -12,7 +12,7 @@ Furthermore, it provides a configuration interface for the whole of the plugin s
 --]]
 
 declare("tcs", {})
-tcs.VERSION = "1.1.1"
+tcs.VERSION = "1.1.2"
 
 --[[ The all important require() function. This is responsible for checking which modules are loaded and
 loading the modules themselves if they need to be loaded. --]]
